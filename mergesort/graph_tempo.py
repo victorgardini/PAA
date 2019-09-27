c21c84bca3932d69ae0be60be390f671f5166c74
Victor Fernandes Gardini <victorfernandesrp@hotmail.com>
2019-09-27 20:26:30 -0300

    mergesort

--- mergesort/graph_tempo.py
+++ mergesort/graph_tempo.py
@@ -5,21 +5,21 @@ tamanho = [10000, 30000, 50000, 70000, 90000, 110000, 130000, 150000] # tempo fi
 
 # ordenado (melhor caso)
 data  = pd.read_csv('ordenado.csv')
-plt.plot(tamanho, data['tempo'], label='Ordenado', color='green')
+plt.plot(tamanho, data['tempo'], label='Ordenado', color='green', linestyle='--')
 
 # ordenado inversamente (pior caso)
 data  = pd.read_csv('ordenadoInversamente.csv')
-plt.plot(tamanho, data['tempo'], label='Ordenado Inversamente', color='red')
+plt.plot(tamanho, data['tempo'], label='Ordenado Inversamente', color='red', linestyle='-')
 
 # desordenado
 data = pd.read_csv('desordenado.csv')
-plt.plot(tamanho, data['tempo'], label='Desordenado', color='blue')
+plt.plot(tamanho, data['tempo'], label='Desordenado', color='blue', linestyle=':')
 
 
 plt.title('Ordenar um vetor pelo algoritmo MergeSort')
 plt.xlabel('Tamanho do vetor (elementos)')
 plt.ylabel('Tempo ordenação (segundos)')
-plt.ylim(ymin=-0.0001)
+#plt.ylim(ymin=-1)
 plt.xlim(xmin=10000)
 plt.legend()
 plt.grid(True)
--- mergesort/melhor_caso.py
+++ mergesort/melhor_caso.py
@@ -5,20 +5,17 @@ import pandas as pd
 
 # (melhor caso)
 data  = pd.read_csv('melhor_caso-f.csv')
-plt.plot(data['x'], data['y'], label='f(n)', color='green')
+plt.plot(data['x'], data['y'], label='f(n)', color='green', linestyle='-')
 
 # ordenado inversamente (pior caso)
 data  = pd.read_csv('melhor_caso-g.csv')
-plt.plot(data['x'], data['y'], label='c.g(n)', color='red')
+plt.plot(data['x'], data['y'], label='c.g(n)', color='red', linestyle='--')
 
-# tn
-data = pd.read_csv('melhor_caso-tn-nanosegundos.csv')
-plt.plot(data['x'], data['y'], label='T(n) em nanosegundos', color='blue')
 
-plt.title('Melhor caso: f(n) e g(n)')
+plt.title('Comparação entre f(n) e g(n)')
 plt.xlabel('Tamanho do vetor (elementos)')
 plt.ylabel('Quantidade de passos (pontos)')
-plt.ylim(ymin=-1)
+#plt.ylim(ymin=-1)
 plt.xlim(xmin=10000)
 plt.legend()
 plt.grid(True)
