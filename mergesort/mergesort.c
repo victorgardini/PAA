f3c8fd50ece46a1c84d35c81b81e2925cb9ae00e
Victor Fernandes Gardini <victorfernandesrp@hotmail.com>
2019-09-29 17:09:58 -0300

    quicksort implementações

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -53,9 +53,7 @@ int * gerarVetorOrdenadoInversamente(int tamanho) {
 void merge(int * vetor, int comeco, int meio, int fim) {
     int com1 = comeco, com2 = meio+1, comAux = 0, tam = fim-comeco+1;
     int * vetAux;
-
     vetAux = (int*)malloc(tam * sizeof(int));
-
     while(com1 <= meio && com2 <= fim){
         if(vetor[com1] < vetor[com2]) {
             vetAux[comAux] = vetor[com1];
@@ -67,27 +65,21 @@ void merge(int * vetor, int comeco, int meio, int fim) {
         }
         comAux++;
     }
-
-
     while(com1 <= meio){  //Caso ainda haja elementos na primeira metade
         vetAux[comAux] = vetor[com1];
         comAux++;
         com1++;
     }
-
-
     while(com2 <= fim) {   //Caso ainda haja elementos na segunda metade
         vetAux[comAux] = vetor[com2];
         comAux++;
         com2++;
     }
-
     for(comAux = comeco; comAux <= fim; comAux++){    //Move os elementos de volta para o vetor original
         vetor[comAux] = vetAux[comAux-comeco];
     }
     free(vetAux);
 }
-
 
 void mergeSort(int * vetor, int comeco, int fim){
     if (comeco < fim) {
