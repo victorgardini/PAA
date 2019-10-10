13e31d349462025859041d4e61ae93c9c95fc019
Victor Fernandes Gardini <victorfernandesrp@hotmail.com>
2019-10-09 21:47:07 -0300

    Adicionando o caso balanceado, finalizando quicksort

--- quicksort/balanceado_caso.py
+++ quicksort/balanceado_caso.py
@@ -4,25 +4,21 @@ import pandas as pd
 # tamanho = [10000, 30000, 50000, 70000, 90000, 110000, 130000, 150000] # tempo fixo
 
 # (melhor caso)
-data  = pd.read_csv('melhor_caso-f.csv')
+data  = pd.read_csv('caso_balanceado-g.csv')
 plt.plot(data['x'], data['y'], label='f(n)', color='green', linestyle='--')
 
 # ordenado inversamente (pior caso)
-data  = pd.read_csv('melhor_caso-g.csv')
-plt.plot(data['x'], data['y'], label='c.g(n)', color='red')
+data  = pd.read_csv('caso_balanceado-f.csv')
+plt.plot(data['x'], data['y'], label='c.g(n)', color='red', linestyle='-')
 
-# tn
-#data = pd.read_csv('melhor_caso-tn-nanosegundos.csv')
-#plt.plot(data['x'], data['y'], label='T(n) em nanosegundos', color='blue')
-
-plt.title('Melhor caso: f(n) e g(n)')
+plt.title('Caso balanceado: f(n) e g(n)')
 plt.xlabel('Tamanho do vetor (elementos)')
 plt.ylabel('Quantidade de passos (pontos)')
 #plt.ylim(ymin=-1)
 plt.xlim(xmin=10000)
 plt.legend()
 plt.grid(color='grey', linestyle=':')
-plt.savefig('GraficoMelhorCasoFnGn.png')
+plt.savefig('GraficoCasoBalanceadoFnGn.png')
 
 
 plt.show()
